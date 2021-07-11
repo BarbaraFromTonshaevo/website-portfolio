f155513780838918d1da69408cf7f4709ac8f2bf
Elkina Barbara <elkinabarbara1999@gmail.com>
2021-07-11 15:18:18 +0300

    evklid - bem, hovers, focuses

--- EVKLID/index.js
+++ EVKLID/index.js
@@ -1,5 +1,15 @@
-window.addEventListener('DOMContentLoaded',function(){
-  document.querySelector('#burger').addEventListener('click',function(){
-    document.querySelector('#menu').classList.toggle('is-active')
+window.addEventListener('DOMContentLoaded', ()=>{
+  document.getElementById('burger').addEventListener('click',()=>{
+    document.getElementById('menu').classList.add('header__menu_is_active');
+  });
+  document.getElementById('menu-close').addEventListener('click',()=>{
+    document.getElementById('menu').classList.remove('header__menu_is_active');
+  });
+  document.querySelectorAll('.hww__steps-btn').forEach(item=>{
+    item.addEventListener('click',()=>{
+      document.querySelector('.hww__info_is_active').classList.remove('hww__info_is_active');
+      console.log(document.querySelector(`.hww__info[data-info="${item.dataset.step}"]`));
+      document.querySelector(`.hww__info[data-info="${item.dataset.step}"]`).classList.add('hww__info_is_active');
+    })
   })
-})
+});
