4871e3e1e4f650c6d23b2864549b0f9bfd79d1f9
Elkina Barbara <elkinabarbara1999@gmail.com>
2021-09-30 13:33:39 +0300

    with new EVKLID

--- EVKLID/index.js
+++ EVKLID/index.js
@@ -1,15 +1,1 @@
-window.addEventListener('DOMContentLoaded', ()=>{
-  document.getElementById('burger').addEventListener('click',()=>{
-    document.getElementById('menu').classList.add('header__menu_is_active');
-  });
-  document.getElementById('menu-close').addEventListener('click',()=>{
-    document.getElementById('menu').classList.remove('header__menu_is_active');
-  });
-  document.querySelectorAll('.hww__steps-btn').forEach(item=>{
-    item.addEventListener('click',()=>{
-      document.querySelector('.hww__info_is_active').classList.remove('hww__info_is_active');
-      console.log(document.querySelector(`.hww__info[data-info="${item.dataset.step}"]`));
-      document.querySelector(`.hww__info[data-info="${item.dataset.step}"]`).classList.add('hww__info_is_active');
-    })
-  })
-});
+window.addEventListener("DOMContentLoaded",()=>{document.getElementById("burger").addEventListener("click",()=>{document.getElementById("menu").classList.add("header__menu_is_active")}),document.getElementById("menu-close").addEventListener("click",()=>{document.getElementById("menu").classList.remove("header__menu_is_active")}),document.querySelectorAll(".hww__steps-btn").forEach(e=>{e.addEventListener("click",()=>{document.querySelector(".hww__info_is_active").classList.remove("hww__info_is_active"),document.querySelector(`.hww__info[data-info="${e.dataset.step}"]`).classList.add("hww__info_is_active")})})});
